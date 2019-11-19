e0a0e273909750c3d173997d9b3e1da94e535e87
Elena Rakhimova <elenara@kth.se>
2019-11-19 15:38:26 +0100

    Added koppsApi connection setup and fetching of syllabusses

---
+++ server/koppsApi.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const log = require('kth-node-log')
+const config = require('./configuration').server
+const redis = require('kth-node-redis')
+const connections = require('kth-node-api-call').Connections
+
+const koppsOpts = {
+  log,
+  https: true,
+  redis,
+  cache: config.cache,
+  timeout: 5000,
+  defaultTimeout: config.koppsApi.defaultTimeout,
+  retryOnESOCKETTIMEDOUT: true,
+  useApiKey: false // skip key
+}
+
+config.koppsApi.doNotCallPathsEndpoint = true // skip checking _paths, because kopps doesnt have it
+config.koppsApi.connected = true
+
+const koppsConfig = {
+  koppsApi: config.koppsApi
+}
+
+const api = connections.setup(koppsConfig, koppsConfig, koppsOpts)
+
+async function getKoppsCourseData(courseCode, semester, language = 'sv') {
+  const { client } = api.koppsApi
+
+  const uri = `${config.koppsApi.basePath}syllabuses/${courseCode}/${semester}?l=${language}`
+  try {
+    return await client.getAsync({ uri, useCache: true })
+  } catch (err) {
+    log.debug('Kopps is not available', err)
+    return err
+  }
+}
+
+module.exports = {
+  koppsApi: api,
+  getKoppsCourseData
+}
