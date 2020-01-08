e337bfb04a67fa89e95c780cbe3dc59a1967ec70
Rickard Falk <ricf@kth.se>
2020-01-08 15:24:34 +0100

    Add show/hide button and icon to sections

--- public/js/app/components/TitleAndInfo.jsx
+++ public/js/app/components/TitleAndInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from 'reactstrap'
+import { FaRegEyeSlash } from 'react-icons/fa'
 import i18n from '../../../../i18n'
 import { InfoModalButton } from '@kth/kth-kip-style-react-components'
 
@@ -15,19 +16,28 @@ const TitleAndInfo = ({ title, shortTitle, visibleInMemo }) => {
   }
 
   return (
-    <span className="title_and_info">
-      <h3 data-testid={shortTitle + '-heading'}>
-        {title}
-        <InfoModalButton
-          modalId={shortTitle + '-infoModal'}
-          modalLabels={infoModalLabels.shortTitle} // use later i18n message: infoModalLabels[shortTitle]
-        />
-      </h3>
-      <p>
-        <i>{visibleInMemo ? 'Visas i kurs-PM' : 'Döljs i kurs-PM'}</i>
-      </p>
-      <Button>{visibleInMemo ? buttons.btn_hide_in_memo : buttons.btn_show_in_memo}</Button>
-    </span>
+    <>
+      <span className="title_and_info">
+        <h3 data-testid={shortTitle + '-heading'}>
+          {title}
+          <InfoModalButton
+            modalId={shortTitle + '-infoModal'}
+            modalLabels={infoModalLabels.shortTitle} // use later i18n message: infoModalLabels[shortTitle]
+          />
+        </h3>
+      </span>
+      <span className="section_info">
+        <span>
+          {visibleInMemo ? null : <FaRegEyeSlash className="section_info_visibility_icon" />}
+          <span className="section_info_visibility_label">
+            {visibleInMemo ? 'Visas i kurs-PM' : 'Döljs i kurs-PM'}
+          </span>
+        </span>
+        <Button style={{ marginTop: 0 }}>
+          {visibleInMemo ? buttons.btn_hide_in_memo : buttons.btn_show_in_memo}
+        </Button>
+      </span>
+    </>
   )
 }
 
