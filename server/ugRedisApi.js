205b24c54c36688bde71e959b70a0b0d82895f8c
Elena Rakhimova <elenara@kth.se>
2020-01-21 10:24:07 +0100

    Minor refactor for course employees

--- server/ugRedisApi.js
+++ server/ugRedisApi.js
@@ -1,6 +1,5 @@
 const serverConfig = require('./configuration').server
 const redis = require('kth-node-redis')
-// const httpResponse = require('kth-node-response')
 const i18n = require('../i18n')
 const log = require('kth-node-log')
 
@@ -10,20 +9,21 @@ const redisKeys = (courseCode, semester, round) => {
     teachers: [`${courseCode}.${semester}.${round}.teachers`],
     examiner: [`${courseCode}.examiner`],
     responsibles: [`${courseCode}.${semester}.${round}.courseresponsible`],
-    assistants: [`${courseCode}.${semester}.${round}.assistants`] //edu.courses.1B.1B1001.19961.1.assistants
+    assistants: [`${courseCode}.${semester}.${round}.assistants`] //edu.courses.SF.SF1624.20191.1.assistants
   }
 }
 
 const createPersonHtml = personList => {
   let personString = ''
-  personList.forEach(person => {
-    personString += `<p class = "person">
+  personList &&
+    personList.forEach(person => {
+      personString += `<p class = "person">
         <i class="fas fa-user-alt"></i>
         <a href="/profile/${person.username}/" target="_blank" property="teach:teacher">
             ${person.givenName} ${person.lastName} 
         </a> 
       </p>  `
-  })
+    })
   console.log('---------- personString ---------', personString)
   return personString
 }
@@ -44,23 +44,27 @@ const minimizePesonalData = personList => {
 async function _getCourseEmployees(courseCode, semester, round = '1') {
   try {
     const { assistants, teachers, examiner, responsibles } = redisKeys(courseCode, semester, round)
-    log.info('_getCourseEmployees with keys: ' + teachers, examiner, responsibles)
+    log.info('_getCourseEmployees with keys: ' + teachers, examiner, responsibles, assistants)
     const ugClient = await redis('ugRedis', serverConfig.cache.ugRedis.redis)
     const arrWithStringifiedArray = await ugClient
       .multi()
       .mget(teachers) // [0]
       .mget(examiner) // [1]
       .mget(responsibles) // [2]
-      // .mget(assistants) //[3]
+      .mget(assistants) //[3]
       .execAsync()
+    console.log(
+      'arrWithStringifiedArrayarrWithStringifiedArrayarrWithStringifiedArray ',
+      arrWithStringifiedArray[3]
+    )
     const flatArrWithHtmlStr = arrWithStringifiedArray
       .flat()
       .map(str => createPersonHtml(JSON.parse(str)))
     return {
       teacher: flatArrWithHtmlStr[0],
       examiner: flatArrWithHtmlStr[1],
-      courseCoordinator: flatArrWithHtmlStr[2]
-      // teacherAssistants: flatArrWithHtmlStr[3]
+      courseCoordinator: flatArrWithHtmlStr[2],
+      teacherAssistants: flatArrWithHtmlStr[3]
     }
   } catch (err) {
     log.error('Exception from ugRedis - multi', { error: err })
