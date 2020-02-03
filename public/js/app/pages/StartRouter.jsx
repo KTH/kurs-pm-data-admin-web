c93dec4fd88b021086c7fa45ab2b4866166bec2f
Elena Rakhimova <elenara@kth.se>
2020-02-03 10:44:32 +0100

    Added page which will be start page for other subpages

---
+++ public/js/app/pages/StartRouter.jsx
@@ -0,0 +1,124 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+/* eslint-disable react/no-danger */
+import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react'
+import { Container, Row, Col, Button, Alert } from 'reactstrap'
+import { Route } from 'react-router-dom'
+import { StickyContainer, Sticky } from 'react-sticky'
+
+import EditorPerTitle from '../components/Editor'
+import Section from '../components/Section'
+import PageHead from '../components/PageHead'
+import ProgressTitle from '../components/ProgressTitle'
+import { context, sections } from '../util/fieldsByType'
+import SideMenu from '../components/SideMenu'
+import i18n from '../../../../i18n'
+import { PageTitle, ProgressBar, ActionModalButton } from '@kth/kth-kip-style-react-components'
+
+@inject(['routerStore'])
+@observer
+class StartRouter extends Component {
+  state = { progress: this.props.progress ? Number(this.props.progress) : 1 }
+
+  courseCode = this.props.routerStore.courseCode
+
+  semester = this.props.routerStore.semester
+
+  componentDidMount() {
+    this.scrollIntoView()
+  }
+
+  doUpdateStates = states => {
+    if (states) this.setState(states)
+  }
+
+  render() {
+    const { pages, pageTitles, actionModals, buttons } = i18n.messages[1]
+    const { title, credits, creditUnitAbbr } = this.props.routerStore.koppsFreshData
+
+    return (
+      <Container className="kip-container" style={{ marginBottom: '115px' }}>
+        <Row>
+          <PageTitle id="mainHeading" pageTitle={pageTitles.new}>
+            <span>
+              {this.courseCode +
+                ' ' +
+                title +
+                ' ' +
+                credits +
+                ' ' +
+                (i18n.isSwedish() ? creditUnitAbbr : 'credits')}
+            </span>
+          </PageTitle>
+        </Row>
+        <ProgressBar active={this.state.progress} pages={pages} />
+        <PageHead semester={this.props.routerStore.semester} />
+        <Row className="mb-4">
+          <Col lg="9">
+            <ProgressTitle
+              id="progress-title"
+              text={pages[this.state.progress - 1].title}
+              infoModalLabels={{
+                header: pages[this.state.progress - 1].title,
+                body: pages[this.state.progress - 1].intro,
+                btnClose: 'Close'
+              }}
+            />
+          </Col>
+          <Col lg="3" className="change-view">
+            <Button className="mt-0 mb-0" onClick={this.toggleViewMode} color="primary" size="sm">
+              {this.state.singleMode
+                ? buttons.btn_switch_view_scroll
+                : buttons.btn_switch_view_single}
+            </Button>
+          </Col>
+        </Row>
+        <StickyContainer className="sticky-content-section">
+          <Row>{/* HERE DIFFERENT CONTENT */}</Row>
+        </StickyContainer>
+        <Container className="fixed-bottom">
+          <Row className="control-buttons">
+            <Row className="w-100 my-0 mx-auto">
+              <Alert isOpen={!!this.state.alertIsOpen}>
+                {this.state.alertText ? this.state.alertText : ''}
+              </Alert>
+            </Row>
+            <Col sm="4" className="step-back">
+              <Button onClick={() => alert('back')} className="btn-back" id="back-to-.." alt="BACK">
+                {buttons.btn_back}
+              </Button>
+            </Col>
+            <Col sm="4" className="btn-cancel">
+              <ActionModalButton
+                btnLabel={buttons.btn_cancel}
+                modalId="cancelStep2"
+                type="cancel"
+                modalLabels={actionModals.infoCancel}
+                onConfirm={() => console.log('Cancelled')}
+              />
+            </Col>
+            <Col sm="4" className="step-forward">
+              <Button onClick={this.handleConfirm} color="secondary">
+                {buttons.btn_save}
+              </Button>
+              <Button
+                onClick={() => alert('Go to granska')}
+                id="to-peview"
+                className="btn-next"
+                style={{ marginLeft: '1.25em' }}
+                color="success"
+                alt={'Go to ' + buttons.btn_preview}
+                disabled={false /* this.state.isError */}
+              >
+                {buttons.btn_preview}
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    )
+  }
+}
+
+export default StartRouter
