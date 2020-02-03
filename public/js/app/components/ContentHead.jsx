1528d120034e045498eeadab70ecda1148c14b22
Elena Rakhimova <elenara@kth.se>
2020-02-03 14:24:45 +0100

    Clean: removing source text from info modal

--- public/js/app/components/ContentHead.jsx
+++ public/js/app/components/ContentHead.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
 import { TitleAndInfoModal } from '@kth/kth-kip-style-react-components'
-import { context } from '../util/fieldsByType'
 import i18n from '../../../../i18n'
 
-const { memoHeadings, buttons, sourceInfo } = i18n.messages[1]
+const { memoHeadings, buttons } = i18n.messages[1]
 
 const ContentHead = ({ contentId }) => {
-  const { header, body } = memoHeadings[contentId]
-  const { source, isRequired } = context[contentId]
-  // ${sourceInfo[source].details}
-  const htmlInfo = `<p><i>${sourceInfo.mandatory[isRequired]}</i></p>
-    ${sourceInfo[source].from}
-    ${body}`
   return (
     <TitleAndInfoModal
       modalId={contentId}
-      titleAndInfo={{ header, body: htmlInfo }}
-      superscript={' ' + source || ''}
+      titleAndInfo={memoHeadings[contentId]}
       btnClose={buttons.btnClose}
     />
   )
