18989db96d655f44f4c7c7f295508447d79512f3
Elena Rakhimova <elenara@kth.se>
2020-02-20 19:09:32 +0100

    Refactor: fetch data from kursPmData Api by using 2 functions, get and create

--- server/kursPmDataApi.js
+++ server/kursPmDataApi.js
@@ -1,73 +1,81 @@
 'use strict'
 
 const log = require('kth-node-log')
-// const config = require('./configuration').server
 const api = require('./api')
 
-async function getMemoByEndPoint(status, memoEndPoint) {
-  const { client, paths } = api.kursPmDataApi
-  const uri = client.resolve(paths.getMemoByEndPoint.uri, { memoEndPoint, status })
+// Gets a list of used round ids for a semester in a course
+async function getMemoApiData(apiFn, uriParam) {
+  // (apiFn, param)
   try {
-    const res = await client.getAsync({ uri })
-    console.log('getMemoByEndPoint', res.body)
+    const { client, paths } = api.kursPmDataApi
+    const uri = client.resolve(paths[apiFn].uri, uriParam)
+    const res = await client.getAsync({ uri, useCache: false })
+    console.log('getMemoApi', apiFn, '======>', res.body)
     return res.body
-  } catch (err) {
-    log.debug('getMemoByEndPoint is not available', err)
-    return err
+  } catch (error) {
+    log.debug('getMemoApi path ', apiFn, ' is not available', error)
+    return error
   }
 }
 
-async function postMemoDataById(courseCode, semester, body) {
+// Gets a list of used round ids for a semester in a course
+async function saveToMemoApi(postApiFn, uriParam, body) {
+  // (apiFn, param)
+  try {
+    const { client, paths } = api.kursPmDataApi
+    const uri = client.resolve(paths[postApiFn].uri, uriParam)
+
+    const res = await client.postAsync({ uri, body, useCache: false })
+    console.log('saveToMemoApi path', postApiFn, '======>', res.body)
+    return res.body
+  } catch (error) {
+    log.debug('saveToMemoApi path ', postApiFn, ' is not available', error)
+    return error
+  }
+}
+
+// Creates copy of published memo and save it as a draft
+// async function createDraftCopyOfPublishedMemo (memoEndPoint) {
+//   const { client, paths } = api.kursPmDataApi
+//   const uri = client.resolve(paths.createDraftCopyOfPublishedMemo.uri, { memoEndPoint })
+//   try {
+//     const res = await client.postAsync({ uri, useCache: false })
+//     return res.body
+//   } catch (err) {
+//     log.debug('createDraftCopyOfPublishedMemo is not available', err)
+//     return err
+//   }
+// }
+
+// POST TOTALLY NEW DRAFT ONLY COURSE, SEMESTER, ROUNDS
+// async function postNewDrafttFromScratch(memoEndPoint, body) {
+//   const { client, paths } = api.kursPmDataApi
+//   const uri = client.resolve(paths.postNewDrafttFromScratch.uri, { memoEndPoint })
+//   try {
+//     const res = await client.postAsync({ uri, body, useCache: false })
+//     return res.body
+//   } catch (err) {
+//     log.debug('postNewDrafttFromScratch is not available', err)
+//     return err
+//   }
+// }
+
+async function updateCreatedDraft(courseCode, semester, body) {
   const { client, paths } = api.kursPmDataApi
   const id = `${courseCode}${semester}`
   const uri = client.resolve(paths.postCourseMemoData.uri, { id })
-  console.log('Prepared whole object is', body)
 
   try {
     const res = await client.postAsync({ uri, body, useCache: false })
-    console.log('!After  postAsync postMemoDataById', res)
     return res.body
   } catch (err) {
-    log.debug('postMemoDataById is not available', err)
+    log.debug('updateCreatedDraft is not available', err)
     return err
   }
 }
 
-// GET DRAFTS
-
-async function getAllMemosByCourseCode(courseCode) {
-  const { client, paths } = api.kursPmDataApi
-  const uri = client.resolve(
-    paths.getAllMemosByCourseCodeAndType.uri,
-    { courseCode, type: 'latest' },
-    { batchSize: 10000 }
-  )
-  try {
-    const res = await client.getAsync({ uri })
-    console.log('getAllMemosByCourseCode', res.body)
-    return res.body
-  } catch (err) {
-    log.debug('getAllMemosByCourseCode is not available', err)
-    return err
-  }
-}
-
-// POST TOTALLY NEW DRAFT ONLY COURSE, SEMESTER, ROUNDS
-async function postNewDrafttFromScratch(courseCode, semester, body) {
-  // updated
-  const { client, paths } = api.kursPmDataApi
-  const uri = client.resolve(paths.postNewDrafttFromScratch.uri, { courseCode, semester })
-  try {
-    const res = await client.postAsync({ uri, body, useCache: false })
-    return res.body
-  } catch (err) {
-    log.debug('postNewDrafttFromScratch is not available', err)
-    return err
-  }
-}
 module.exports = {
-  getMemoByEndPoint,
-  getAllMemosByCourseCode, // first step to get drafts
-  postMemoDataById,
-  postNewDrafttFromScratch
+  getMemoApiData,
+  saveToMemoApi,
+  updateCreatedDraft
 }
