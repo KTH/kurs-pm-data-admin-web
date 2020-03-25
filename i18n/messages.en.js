68c9608a568fdc85c68b900d2c33c5c64d903e82
Elena Rakhimova <elenara@kth.se>
2020-03-25 19:12:21 +0100

    Added progress steps back

--- i18n/messages.en.js
+++ i18n/messages.en.js
@@ -214,7 +214,7 @@ module.exports = {
   },
   pages: [
     {
-      title: 'Choose course offering',
+      title: '1.Choose course offering',
       intro: `Choose a semester and a course offering for thecourse memo to be published (step 1 of 3). 
       In this step you can choose to create a new coursememo for the course offering or you can chooseto copy a draft from a previously published course memo for this course. 
       In the next step (2 of 3) you can edit the course memo. 
@@ -222,12 +222,12 @@ module.exports = {
       `
     },
     {
-      title: 'Edit course memo',
+      title: '2.Edit course memo',
       intro: `In this step (2 of 3) course memo data and course memo shall be uploaded, 
       changes to the chosen course offering is summarized and some of the course data are reviewed and adjusted.`
     },
     {
-      title: 'Review and publish',
+      title: '3.Review and publish',
       intro: `In this step (3 of 3) a preview of the course memo data with course data is presented as it will be published on the page .... 
       It is possible to go back to make adjustments, to save a draft or publish the information.`
     }
