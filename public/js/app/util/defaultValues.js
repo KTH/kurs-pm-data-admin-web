7378c8c6a87130c4a801dccc0f626e5bf5799d75
Elena Rakhimova <elenara@kth.se>
2020-04-01 18:02:32 +0200

    Moved and refactor: code for temporarily default values for editor for a schedule details

---
+++ public/js/app/util/defaultValues.js
@@ -0,0 +1,52 @@
+/* eslint-disable import/prefer-default-export */
+/* Here are temporary default values for editor's content */
+const SCHEDULE_TITLES = {
+  0: ['Learning activities', 'Content', 'Preparations'],
+  1: ['Läraktivitet', 'Innehåll', 'Förberedelse']
+}
+
+const _scheduleDetailsTemplate = language => {
+  const langIndex = language === 'en' ? 0 : 1
+  const header = SCHEDULE_TITLES[langIndex]
+    .map(
+      thTitle => `<th style="width: 33.3333%">${thTitle}
+  </th>`
+    )
+    .join('')
+
+  const emptyRow = `<tr>
+    ${'<td style="width: 33.3333%;">&nbsp;</td>'.repeat(3)}
+    </tr>`
+
+  const scheduleTable = `<table style="border-collapse: collapse; width: 100%;" border="1">
+    <thead><tr>
+    ${header}
+    </tr></thead>
+    <tbody>
+    ${emptyRow.repeat(3)}
+    </tbody>
+    </table>
+    `
+  return scheduleTable
+}
+
+const _scheduleLinks = (url, language) => {
+  const english = language === 'en'
+  const scheduleName = english ? 'Schedule' : 'Schema'
+  if (!url) return ''
+  const urls = Array.isArray(url) ? url : [url]
+  const uniqueUrls = urls.filter((value, index, self) => self.indexOf(value) === index)
+  return uniqueUrls
+    .map(uniqueUrl =>
+      uniqueUrl
+        ? `<br/><a title="${scheduleName}" href="${uniqueUrl}" target="_blank" rel="noopener">${scheduleName}</a>`
+        : ''
+    )
+    .join('')
+}
+
+export const combineScheduleValues = (schemaUrls, memoLangAbbr) => {
+  return schemaUrls && schemaUrls.length > 0
+    ? `${_scheduleDetailsTemplate(memoLangAbbr)}<p>${_scheduleLinks(schemaUrls)}</p>`
+    : `${_scheduleDetailsTemplate(memoLangAbbr)}`
+}
