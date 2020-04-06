047b9b5c8492caa87e23e4ec53940e6f2d9a01e0
Rickard Falk <ricf@kth.se>
2020-04-06 08:44:19 +0200

    Cleanup of component PreviewContainer

--- public/js/app/pages/PreviewContainer.jsx
+++ public/js/app/pages/PreviewContainer.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-console */
-/* eslint-disable react/no-danger */
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
 import { Container, Row } from 'reactstrap'
@@ -8,12 +5,8 @@ import ControlPanel from '../components/ControlPanel'
 import i18n from '../../../../i18n'
 import { PageTitle, ProgressBar } from '@kth/kth-kip-style-react-components'
 import ProgressTitle from '../components/ProgressTitle'
-// import { Switch,Route } from 'react-router-dom'
-import MemoEdition from './MemoEdition'
 import PageHead from '../components/PageHead'
-import axios from 'axios'
 
-const ADMIN = '/kursinfoadmin/kurs-pm-data/'
 const PROGRESS = 3
 
 @inject(['routerStore'])
@@ -26,18 +19,6 @@ class PreviewContainer extends Component {
 
   langIndex = this.props.routerStore.langIndex
 
-  componentDidMount() {
-    console.log('parent state', this.state.previewMemo.ladokRoundIds)
-  }
-
-  componentDidUpdate() {
-    console.log('parent state did update', this.state.previewMemo)
-  }
-
-  doUpdateStates = states => {
-    if (states) this.setState(states)
-  }
-
   handleAlert = alertText => {
     this.setState({ alertIsOpen: true, alertText })
     setTimeout(() => {
@@ -45,103 +26,42 @@ class PreviewContainer extends Component {
     }, 2000)
   }
 
-  onSave = (editorContent, alertType) => {
-    const { alerts } = i18n.messages[this.langIndex]
-    const { memoEndPoint } = editorContent
-    const { examinationModules, scheduleDetails } = editorContent // because koppsFreshData contains them as well
-    const body = {
-      ...editorContent, // containt kopps old data, or it is empty first time
-      ...this.props.routerStore.koppsFreshData, // update memo data with fresh kopps data or fill in empty data if it's first time
-      ...examinationModules, // update it again from editor content because it was overriden by koppsFreshData default values
-      ...scheduleDetails // update it again from editor content because it was overriden by koppsFreshData default values
-    }
-    this.doUpdateStates({ updatedMemo: editorContent })
-    console.log('Content is submited to parent, preparing to save changes:', body)
-    return axios
-      .post('/kursinfoadmin/kurs-pm-data/internal-api/draft-updates/' + memoEndPoint, body)
-      .then(() => this.props.routerStore.tempMemoData(body))
-      .then(() => this.handleAlert(alerts[alertType]))
-      .catch(error => console.log(error)) // alert error
-  }
-
-  /** * Conrol Panel ** */
-
-  /** * User clicked button to save a draft  ** */
-  handleBtnSave = () => {
-    const res = this.onSave(this.state.previewMemo, 'autoSaved')
-    return res
-  }
-
-  handleBackSave = () => {
-    const { courseCode, memoEndPoint } = this.state.previewMemo
-    this.handleBtnSave().then(
-      setTimeout(() => {
-        window.location = `${ADMIN}${courseCode}?memoEndPoint=${memoEndPoint}`
-      }, 500)
+  courseSubHeader = () => {
+    const { title, credits, creditUnitAbbr } = this.props.routerStore.koppsFreshData
+    return (
+      <span>
+        {this.state.previewMemo.courseCode +
+          ' ' +
+          title +
+          ' ' +
+          credits +
+          ' ' +
+          (i18n.isSwedish() ? creditUnitAbbr : 'credits')}
+      </span>
     )
-  }
-
-  /** * User clicked button to go to next step  ** */
-  onContinue = () => {
-    switch (this.state.progress) {
-      default:
-        this.setState({ progress: this.state.progress + 1 })
-        break
-      case 3:
-        alert('PUBLISHED')
-        break
-    }
-  }
-
-  /** * User clicked button to go to one step back ** */
-  onBack = () => {
-    switch (this.state.progress) {
-      case 2:
-        // TODO: ERROR HANTERING
-        this.handleBackSave()
-        break
-      default:
-        this.setState({ progress: this.state.progress - 1 })
-        break
-    }
   }
 
   render() {
     const { progressTitleHeaders, progressBarHeaders, pageTitles } = i18n.messages[this.langIndex]
-    const { title, credits, creditUnitAbbr } = this.props.routerStore.koppsFreshData
     const { memoName, semester = '' } = this.state.previewMemo
 
     return (
       <Container className="kip-container" style={{ marginBottom: '115px' }}>
         <Row>
           <PageTitle id="mainHeading" pageTitle={pageTitles.preview}>
-            <span>
-              {this.state.previewMemo.courseCode +
-                ' ' +
-                title +
-                ' ' +
-                credits +
-                ' ' +
-                (i18n.isSwedish() ? creditUnitAbbr : 'credits')}
-            </span>
+            {this.courseSubHeader()}
           </PageTitle>
         </Row>
         <ProgressBar active={this.state.progress} pages={progressBarHeaders} />
         <Row />
         <PageHead semester={semester} memoName={memoName} />
-        {
-          {
-            2: <MemoEdition onSave={this.onSave} onChange={this.doUpdateStates} />,
-            3: <ProgressTitle id="progress-title" text={progressTitleHeaders[PROGRESS - 1]} />
-          }[this.state.progress]
-        }
+        <ProgressTitle id="progress-title" text={progressTitleHeaders[PROGRESS - 1]} />
         <Row style={{ borderTop: '2px solid rgb(212,212,212)' }} />
         <Container className="fixed-bottom">
           <ControlPanel
             langIndex={this.langIndex}
-            onSubmit={this.onContinue}
-            onSave={this.handleBtnSave}
-            onBack={this.onBack}
+            onSubmit={() => this.handleAlert('Publish option is not yet implemented!')}
+            onBack={() => this.handleAlert('Back option is not yet implemented!')}
             progress={this.state.progress}
             alertText={this.state.alertText}
             alertIsOpen={this.state.alertIsOpen}
