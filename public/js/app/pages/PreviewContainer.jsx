716f7ff293bdc552a85058c4e9ddf88d4396d998
Rickard Falk <ricf@kth.se>
2020-05-05 13:41:15 +0200

    Correction to resolve course images

--- public/js/app/pages/PreviewContainer.jsx
+++ public/js/app/pages/PreviewContainer.jsx
@@ -21,6 +21,9 @@ const PROGRESS = 3
 
 // Logic copied from kursinfo-web
 export const resolveCourseImage = (imageFromAdmin, courseMainSubjects = '', language) => {
+  const englishTranslations = i18n.messages[0].messages
+  const swedishTranslations = i18n.messages[1].messages
+
   let courseImage = ''
   // If course administrator has set own picture, use that
   if (imageFromAdmin && imageFromAdmin.length > 4) {
