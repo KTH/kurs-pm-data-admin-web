1d4d35b1516967fb262faa26c0e2a97108e2621b
Elena Rakhimova <elenara@kth.se>
2020-06-16 16:30:31 +0200

    Refactor: use new component

--- public/js/app/app.jsx
+++ public/js/app/app.jsx
@@ -12,7 +12,7 @@ import RouterStore from './stores/RouterStore'
 
 // Pages
 import ChangePublished from './pages/ChangePublished'
-import ChoiceOptions from './pages/ChoiceOptions'
+import CreateNewOptions from './pages/CreateNewOptions'
 import MemoContainer from './pages/MemoContainer'
 import PreviewContainer from './pages/PreviewContainer'
 
@@ -31,7 +31,7 @@ function appFactory() {
           path="/kursinfoadmin/kurs-pm-data/published/:courseCode/"
           component={ChangePublished}
         />
-        <Route exact path="/kursinfoadmin/kurs-pm-data/:courseCode/" component={ChoiceOptions} />
+        <Route exact path="/kursinfoadmin/kurs-pm-data/:courseCode/" component={CreateNewOptions} />
         <Route
           exact
           path="/kursinfoadmin/kurs-pm-data/:courseCode/:memoEndPoint"
