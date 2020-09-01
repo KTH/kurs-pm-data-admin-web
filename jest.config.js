e8a340709c651a741719a5cb93c5c41f258a0ce3
Elena Rakhimova <elenara@kth.se>
2020-09-01 16:15:19 +0200

    Fixed config for babel and jest to define its area and preset

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  transformIgnorePatterns: ['.*/node_modules/(?!kth/kth-kip-style-react-components/.*)']
+  globals: {
+    NODE_ENV: 'test'
+  },
+  clearMocks: true,
+  notifyMode: 'failure-change',
+  transformIgnorePatterns: ['node_modules/(?!(@kth|@babel|@jest)/)'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node']
 }
