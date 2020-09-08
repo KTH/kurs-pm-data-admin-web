0b211458d6bc19d6b6583f3dfd5a86d3674e3662
Elena Rakhimova <elenara@kth.se>
2020-09-08 15:35:45 +0200

    Remove combination functions from router store and client side

--- public/js/app/stores/RouterStore.js
+++ public/js/app/stores/RouterStore.js
@@ -1,5 +1,4 @@
 import { observable, action } from 'mobx'
-import { combineScheduleValues } from '../util/defaultValues'
 import axios from 'axios'
 import { SERVICE_URL } from '../util/constants'
 
@@ -29,49 +28,6 @@ class RouterStore {
     this.courseCode = props.courseCode.toUpperCase() || ''
     this.memoEndPoint = props.memoEndPoint.toUpperCase() || ''
     this.memoLangAbbr = props.memoLangAbbr || 'sv'
-  }
-
-  @action combineDefaultValues() {
-    const {
-      examinationSubSection,
-      equipment,
-      scheduleDetails,
-      literature,
-      possibilityToCompletion,
-      possibilityToAddition
-    } = this.memoData
-    this.memoData = {
-      ...this.memoData,
-      examinationSubSection: examinationSubSection || this.koppsFreshData.examinationModules || '', // koppsFreshData.examinationModules
-      // eslint-disable-next-line no-use-before-define
-      equipment: equipment || this.koppsFreshData.equipmentTemplate || '',
-      scheduleDetails:
-        scheduleDetails ||
-        combineScheduleValues(this.koppsFreshData.schemaUrls, this.memoLangAbbr) ||
-        '',
-      literature: literature || this.koppsFreshData.literatureTemplate || '',
-      possibilityToCompletion:
-        possibilityToCompletion || this.koppsFreshData.possibilityToCompletionTemplate || '',
-      possibilityToAddition:
-        possibilityToAddition || this.koppsFreshData.possibilityToAdditionTemplate || ''
-    }
-  }
-
-  @action removeTemplatesFromKoppsFreshData() {
-    ;[
-      'equipmentTemplate',
-      'literatureTemplate',
-      'possibilityToCompletionTemplate',
-      'possibilityToAdditionTemplate'
-    ].map((property) => delete this.koppsFreshData[property])
-  }
-
-  @action updateMemoDataWithFreshKoppsData() {
-    this.memoData = { ...this.memoData, ...this.koppsFreshData }
-  }
-
-  @action tempMemoData(memoData) {
-    this.memoData = memoData
   }
 
   async _filterOutUsedRounds(usedRoundsThisTerm, chosenSemester, koppsLastTerms = null) {
