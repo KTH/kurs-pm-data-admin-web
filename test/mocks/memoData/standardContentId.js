de90ea36065b76b0e1fb7e0c68a02009345e5975
Elena Rakhimova <elenara@kth.se>
2020-09-14 11:15:57 +0200

    Hardcoded standard content header and test it compare to generated

--- test/mocks/memoData/standardContentId.js
+++ test/mocks/memoData/standardContentId.js
@@ -1,5 +1,46 @@
 import { allStandardHeadersAndSubHd } from '../../../public/js/app/util/fieldsByType'
 
-const standardMemoContent = allStandardHeadersAndSubHd()
+const standardMemoContent = [
+  'additionalRegulations',
+  'communicationDuringCourse',
+  'courseContent',
+  'courseCoordinator',
+  'equipment',
+  'ethicalApproach',
+  'ethicalApproachSubSection',
+  'examination',
+  'examinationSubSection',
+  'examiner',
+  'gradingCriteria',
+  'gradingScale',
+  'infoForReregisteredStudents',
+  'learningActivities',
+  'learningOutcomes',
+  'literature',
+  'otherContacts',
+  'otherRequirementsForFinalGrade',
+  'permanentDisability',
+  'permanentDisabilitySubSection',
+  'possibilityToAddition',
+  'possibilityToCompensate',
+  'possibilityToCompletion',
+  'preparations',
+  'prerequisites',
+  'reportingResults',
+  'scheduleDetails',
+  'software',
+  'teacher',
+  'teacherAssistants'
+]
+
+describe('Contol generation of headers which are standard for memo content', () => {
+  test('Check standard memo content', (done) => {
+    const s = allStandardHeadersAndSubHd()
+
+    expect(standardMemoContent).toStrictEqual(s)
+
+    done()
+  })
+})
 
 export default standardMemoContent
