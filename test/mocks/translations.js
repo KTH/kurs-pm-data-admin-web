5c46a0df5bb870faa82003cda9cdacb754509705
Elena Rakhimova <elenara@kth.se>
2020-09-14 12:06:44 +0200

    tranlations mock for important texts

--- test/mocks/translations.js
+++ test/mocks/translations.js
@@ -7,6 +7,29 @@ const translations = {
       aboutCourse: 'Om kursen',
       noLinksInPreview: 'Länkar i menyn fungerar inte i granska-läge'
     },
+    buttons: {
+      /** ************ BUTTONS ****************** */
+      btnAddExtra: 'Add heading to ',
+      btnClose: 'Close',
+      btnRemove: 'Delete draft',
+      btnRemoveHeader: 'Remove added header',
+      btnFinish: 'Exit',
+      btnSaveAndFinish: 'Exit (save draft)',
+      closeEditor: 'Close edit mode',
+      preview: 'Preview',
+      previewPdf: 'Preview PDF',
+      edit: 'Edit',
+      cancel: 'Cancel',
+      save: 'Save',
+      saveDraft: 'Save draft',
+      publish: 'Publish',
+      goToRounds: 'Choose course offering',
+      save_and_cancel: 'Save draft and cancel',
+      btn_copy: 'Copy link to preview',
+      btn_switch_view_single: 'Switch to “Single View”',
+      btn_switch_view_scroll: 'Switch to “Scroll View”',
+      showGuidance: 'Show guidance'
+    },
     labelContacts: {
       courseContactsTitle: 'Contacts',
       communicationWithTeachersTitle: 'Communication With Teachers',
@@ -26,6 +49,41 @@ const translations = {
       versionTitle: 'Course memo version',
       courseMemoPdf: 'Course memo pdf',
       syllabus: 'Syllabus'
+    },
+    memoTitlesByMemoLang: {
+      additionalRegulations: 'Additional regulations',
+      commentAboutMadeChanges: 'Made changes',
+      communicationDuringCourse: 'Communication during course',
+      courseContent: 'Course contents',
+      courseCoordinator: 'Course coordinator',
+      ethicalApproach: 'Ethical approach',
+      equipment: 'Equipment',
+      examiner: 'Examiner',
+      examination: 'Examination',
+      extraHeaders1: 'Extra header 1',
+      extraHeaders2: 'Extra header 2',
+      extraHeaders3: 'Extra header 3',
+      extraHeaders4: 'Extra header 4',
+      gradingCriteria: 'Grading criteria/assessment criteria',
+      gradingScale: 'Grading scale',
+      infoForReregisteredStudents: 'Changes of the course before this course offering',
+      learningActivities: 'Learning activities',
+      learningOutcomes: 'Intended learning outcomes',
+      literature: 'Literature',
+      otherContacts: 'Other contacts',
+      otherRequirementsForFinalGrade: 'Other requirements for final grade',
+      permanentDisability: 'Support for students with disabilities',
+      possibilityToAddition: 'Opportunity to raise an approved grade via renewed examination',
+      possibilityToCompletion:
+        'Opportunity to complete the requirements via supplementary examination',
+      possibilityToCompensate: 'Alternatives to missed activities or tasks',
+      preparations: 'Specific preparations',
+      prerequisites: 'Recommended prerequisites',
+      reportingResults: 'Reporting of exam results',
+      scheduleDetails: 'Detailed plan',
+      software: 'Software',
+      teacher: 'Teacher',
+      teacherAssistants: 'Teacher assistants'
     },
     orderedFilledInAndVisible: [
       'Course contents',
@@ -58,6 +116,12 @@ const translations = {
       'Created by user First header for section extraHeaders4',
       'Created by user Second header for section extraHeaders4'
     ],
+    pageTitles: {
+      new: 'Create and publish course memo',
+      // draft: 'Publish course memo draft',
+      published: 'Edit published course memo',
+      preview: 'Create and publish course memo'
+    },
     sectionsLabels: {
       contentAndOutcomes: 'Content and learning outcomes',
       prep: 'Preparations before course start',
@@ -66,7 +130,47 @@ const translations = {
       contacts: 'Contact'
     }
   },
-  sv: {}
+  sv: {
+    buttons: {
+      /** ************ BUTTONS ****************** */
+      btnAddExtra: 'Lägg till rubrik till ',
+      btnClose: 'Stäng',
+      btnRemove: 'Radera utkast',
+      btnRemoveHeader: 'Ta bort tillagd rubrik',
+      btnFinish: 'Avsluta',
+      btnSaveAndFinish: 'Avsluta med utkast',
+      closeEditor: 'Stäng redigeringsläge',
+      preview: 'Granska',
+      previewPdf: 'Granska PDF',
+      edit: 'Redigera',
+      cancel: 'Avbryt',
+      save: 'Spara',
+      saveDraft: 'Spara utkast',
+      publish: 'Publicera',
+      goToRounds: 'Välj kursomgång',
+      save_and_cancel: 'Spara utkast och avsluta',
+      btn_copy: 'Kopiera länk till utkast',
+      btn_switch_view_single: 'Ändra till ”Fokus-vy”',
+      btn_switch_view_scroll: 'Ändra till ”Översikts-vy”',
+      showGuidance: 'Visa vägledning'
+    },
+    notIncludedInMemoYet: {
+      section: 'Innehåll finns inlagt. Välj: Inkludera för att visa rubrik med innehåll i kurs-PM',
+      subSection: 'Innehåll finns inlagt. Välj: Inkludera avsnitt för att visa innehållet i kurs-PM'
+    },
+    pageTitles: {
+      new: 'Skapa och publicera kurs-PM',
+      published: 'Ändra publicerat kurs-PM',
+      preview: 'Skapa och publicera kurs-PM'
+    },
+    sectionsLabels: {
+      contentAndOutcomes: 'Innehåll och lärandemål',
+      prep: 'Förbereda inför kursstart',
+      reqToFinal: 'Examination och slutförande',
+      extra: 'Ytterligare information',
+      contacts: 'Kontakter'
+    }
+  }
 }
 
 export default translations
