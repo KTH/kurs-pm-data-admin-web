75f624a1ffaad5ea090c753a88acb0078f1d372b
Elena Rakhimova <elenara@kth.se>
2020-09-14 11:14:17 +0200

    Added translations for test

---
+++ test/mocks/translations.js
@@ -0,0 +1,72 @@
+const translations = {
+  en: {
+    breadCrumbLabels: {
+      university: 'KTH',
+      student: 'Student på KTH',
+      directory: 'Kurs- och programkatalogen',
+      aboutCourse: 'Om kursen',
+      noLinksInPreview: 'Länkar i menyn fungerar inte i granska-läge'
+    },
+    labelContacts: {
+      courseContactsTitle: 'Contacts',
+      communicationWithTeachersTitle: 'Communication With Teachers',
+      courseCoordinatorTitle: 'Course Coordinator',
+      teacherTitle: 'Teacher',
+      teacherAssistantsTitle: 'Teacher Assistants',
+      examinerTitle: 'Examiner',
+      otherContactsTitle: 'Other Contacts'
+    },
+    labelFacts: {
+      languageOfInstructionTitle: 'Language Of Instruction',
+      offeredByTitle: 'Offered By',
+      roundsTitle: 'Course offering'
+    },
+    labelLinks: {
+      courseMemoArchiveLabel: 'Course memo archive',
+      versionTitle: 'Course memo version',
+      courseMemoPdf: 'Course memo pdf',
+      syllabus: 'Syllabus'
+    },
+    orderedFilledInAndVisible: [
+      'Course contents',
+      'Intended learning outcomes',
+      'Learning activities',
+      'Detailed plan',
+      'Created by user First header for section extraHeaders1',
+      'Created by user Second header for section extraHeaders1',
+      'Recommended prerequisites',
+      'Specific preparations',
+      'Literature',
+      'Equipment',
+      'Software',
+      'Support for students with disabilities',
+      'Created by user First header for section extraHeaders2',
+      'Created by user Second header for section extraHeaders2',
+      'Grading scale',
+      'Examination',
+      'Other requirements for final grade',
+      'Grading criteria/assessment criteria',
+      'Opportunity to complete the requirements via supplementary examination',
+      'Opportunity to raise an approved grade via renewed examination',
+      'Alternatives to missed activities or tasks',
+      'Reporting of exam results',
+      'Ethical approach',
+      'Created by user First header for section extraHeaders3',
+      'Created by user Second header for section extraHeaders3',
+      'Additional regulations',
+      'Changes of the course before this course offering',
+      'Created by user First header for section extraHeaders4',
+      'Created by user Second header for section extraHeaders4'
+    ],
+    sectionsLabels: {
+      contentAndOutcomes: 'Content and learning outcomes',
+      prep: 'Preparations before course start',
+      reqToFinal: 'Examination and completion',
+      extra: 'Further information',
+      contacts: 'Contact'
+    }
+  },
+  sv: {}
+}
+
+export default translations
