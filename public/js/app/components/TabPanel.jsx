b631a97fbc549cc9fc24f471dfe67816ef52bfdf
Elena Rakhimova <elenara@kth.se>
2021-01-22 16:18:13 +0100

    feat: add component tab panel for editing memo

---
+++ public/js/app/components/TabPanel.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TabPanel = ({ activeTabId, onClick, sections, sectionsLabels }) => (
+  <div className="tab-bar-container row">
+    <div className="col">
+      <ul className="nav nav-tabs" id="sectionsTabPanel" role="tablist">
+        {sections.map(({ id }) => (
+          <li className="nav-item" key={'header-' + id}>
+            <a
+              className={`nav-link ${activeTabId === id ? 'active' : ''}`}
+              id={id + '-tab'}
+              data-toggle="tab"
+              href={`#${id}`}
+              role="tab"
+              aria-controls={id}
+              aria-selected={activeTabId === id}
+              onClick={() => onClick(id)}
+            >
+              {sectionsLabels[id]}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+)
+TabPanel.propTypes = {
+  activeTabId: PropTypes.string,
+  onClick: PropTypes.func,
+  sections: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+  sectionsLabels: PropTypes.objectOf(PropTypes.string)
+}
+
+export default TabPanel
