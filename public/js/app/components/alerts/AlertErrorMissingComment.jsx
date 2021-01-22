85be2b531121b9ac84a326bcbd384283f7bad50d
Elena Rakhimova <elenara@kth.se>
2021-01-22 16:20:40 +0100

    refactor: separate component for error alert if textarea comment about changes is empty

---
+++ public/js/app/components/alerts/AlertErrorMissingComment.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import UpperAlertRow from './UpperAlertRow'
+
+const AlertErrorMissingComment = ({ isError, alertMsg }) =>
+  isError && (
+    <UpperAlertRow alertMsg={alertMsg} color="danger" ukey="error-if-smth-missing-upper-alert" />
+  )
+
+AlertErrorMissingComment.propTypes = {
+  alertMsg: PropTypes.string,
+  isError: PropTypes.bool
+}
+
+export default AlertErrorMissingComment
