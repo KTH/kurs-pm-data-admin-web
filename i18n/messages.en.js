1fe88777c143208e5631e1a6870347d8a45eb4ac
Elena Rakhimova <elenara@kth.se>
2021-02-09 16:21:15 +0100

    clean: remove [TO-ENG] before user tests

--- i18n/messages.en.js
+++ i18n/messages.en.js
@@ -541,15 +541,15 @@ module.exports = {
   sectionsSummary: {
     about: 'About',
     contentAndOutcomes:
-      '[TO-ENG] Rubriker under Innehåll och lärandemål syftar dels till att övergripande beskriva innehåll och lärandemål för kursen (hämtas från kursplanen) dels till att mer detaljerat beskriva vilka läraktiviteter som ingår i kursen. Sist har du möjlighet att skapa en detaljplanering.',
+      'Rubriker under Innehåll och lärandemål syftar dels till att övergripande beskriva innehåll och lärandemål för kursen (hämtas från kursplanen) dels till att mer detaljerat beskriva vilka läraktiviteter som ingår i kursen. Sist har du möjlighet att skapa en detaljplanering.',
     prep:
-      '[TO-ENG] Rubriker under Förbereda inför kursstart syftar till att informera studenten om vilka förberedelser som studenten bör göra innan kursstart. Utöver vilken kurslitteratur som ingår kan till exempel en specifik programvara eller annan utrustning komma att användas under kursen',
+      'Rubriker under Förbereda inför kursstart syftar till att informera studenten om vilka förberedelser som studenten bör göra innan kursstart. Utöver vilken kurslitteratur som ingår kan till exempel en specifik programvara eller annan utrustning komma att användas under kursen',
     reqToFinal:
-      '[TO-ENG] Rubriker under Examination och slutförande syftar till att tydliggöra för studenten hur examinationen kommer att genomföras och hur betygsättningen genomförs. Till exempel kan studenten få information möjlighet till komplettering, plussning och ersättningsuppgifter.',
+      'Rubriker under Examination och slutförande syftar till att tydliggöra för studenten hur examinationen kommer att genomföras och hur betygsättningen genomförs. Till exempel kan studenten få information möjlighet till komplettering, plussning och ersättningsuppgifter.',
     extra:
-      '[TO-ENG] Rubriker under Ytterligare information syftar till att beskriva andra föreskrifter i kursplanen som inte ryms under övriga rubriker i kursplanen. Dessutom kan man här beskriva relevanta slutsatser från föregående kursvärderingar och kursanalyser samt eventuella ändringar som har införts till denna kursomgång.',
+      'Rubriker under Ytterligare information syftar till att beskriva andra föreskrifter i kursplanen som inte ryms under övriga rubriker i kursplanen. Dessutom kan man här beskriva relevanta slutsatser från föregående kursvärderingar och kursanalyser samt eventuella ändringar som har införts till denna kursomgång.',
     contacts:
-      '[TO-ENG] Rubriker under Kontakter syftar till att informera studenten om hur kommunikationen med lärare och annan relevant personal inför och under kursomgångens genomförande sker. Med tydligt beskrivna instruktioner kommer kommunikationen blir mer effektiv för både studenter och lärare på kursen.'
+      'Rubriker under Kontakter syftar till att informera studenten om hur kommunikationen med lärare och annan relevant personal inför och under kursomgångens genomförande sker. Med tydligt beskrivna instruktioner kommer kommunikationen blir mer effektiv för både studenter och lärare på kursen.'
   },
   pageTitles: {
     new: 'Create and publish course memo',
@@ -742,26 +742,26 @@ module.exports = {
       2: 'Autumn '
     },
     summaryIntroductionHelp: {
-      titleMain: '[TO-ENG] Introduktion och hjälp',
-      startInfo: `[TO-ENG] <p>Innan du skapar ditt kurs-PM behöver du förståelse för kursinformation i stort och kurs-PM i synnerhet. 
+      titleMain: 'Introduktion och hjälp',
+      startInfo: `<p>Innan du skapar ditt kurs-PM behöver du förståelse för kursinformation i stort och kurs-PM i synnerhet. 
       Du kan även behöva se över ditt upplägg i Canvas så det fungerar bra i hop med kurs-PM. </p>`,
       aboutMemo: {
-        title: '[TO-ENG] Vad är ett kurs-PM?',
+        title: 'Vad är ett kurs-PM?',
         details:
-          '<p>[TO-ENG] Kurs-PM är planen för kursomgångens genomförande. Ett kurs-PM ska hjälpa studenterna att förbereda och planera för kursens genomförande. Kurs-PM innehåller information om kursens mål, läraktiviteter, förberedelser, detaljer om examination och kontakter. Det ska också inkludera information om studentens rättigheter och skyldigheter på kursen. Ett kurs-PM guidar studenten och möjliggör att kursen kan genomföras på ett effektivt och ändamålsenligt sätt.</p>'
+          '<p>Kurs-PM är planen för kursomgångens genomförande. Ett kurs-PM ska hjälpa studenterna att förbereda och planera för kursens genomförande. Kurs-PM innehåller information om kursens mål, läraktiviteter, förberedelser, detaljer om examination och kontakter. Det ska också inkludera information om studentens rättigheter och skyldigheter på kursen. Ett kurs-PM guidar studenten och möjliggör att kursen kan genomföras på ett effektivt och ändamålsenligt sätt.</p>'
       },
       aboutKursinformation: {
-        title: '[TO-ENG] Olika delar av kursinformation',
-        details: `<p>[TO-ENG] Delar av den information som visas på Om kursen hämtas från olika källor. En källa är utbildningsdatabasen Kopps, där kurstillfällen, kursens kursplan och ”kursgemensamma uppgifter” finns. Även när du arbetar med ditt kurs-PM hämtas vissa av dessa uppgifter från Kopps till ditt kurs-PM.</p>
+        title: 'Olika delar av kursinformation',
+        details: `<p>Delar av den information som visas på Om kursen hämtas från olika källor. En källa är utbildningsdatabasen Kopps, där kurstillfällen, kursens kursplan och ”kursgemensamma uppgifter” finns. Även när du arbetar med ditt kurs-PM hämtas vissa av dessa uppgifter från Kopps till ditt kurs-PM.</p>
           <p>Finns beskrivande text för kursen inlagd i Kopps visas den på Om kursen, kursansvarig och examinator kan dock lägga in en annan beskrivande text via Om kursens administrationsverktyg ”Administrera om kursen”.</p>
           <p>En annan källa är Ladok, där information om bland annat förstagångsregistrerade studenter och resultat för tidigare kursomgångar finns.</p>
           <p>På Om kursen ska även kursens kursanalyser laddas upp.</p>`
       },
       aboutHelpInCanvasAndMemo: {
-        title: '[TO-ENG] Få hjälp med att strukturera information i Canvas och Kurs-PM',
+        title: 'Få hjälp med att strukturera information i Canvas och Kurs-PM',
         details: ''
       },
-      mainContinue: `<p>[TO-ENG] På denna sida hittar du introduktion för varje avsnitt och rubrik. Börja med att förstå de olika delarna i ett kurs-PM. Det gör du genom att gå igenom de olika flikarna/ tabbarna som delar in kurs-PM:et i avsnitt. Under varje flik finns en expanderbar länk som förklarar vad avsnittet syftar till och vilka rubriker som hör hit.</p>
+      mainContinue: `<p>På denna sida hittar du introduktion för varje avsnitt och rubrik. Börja med att förstå de olika delarna i ett kurs-PM. Det gör du genom att gå igenom de olika flikarna/ tabbarna som delar in kurs-PM:et i avsnitt. Under varje flik finns en expanderbar länk som förklarar vad avsnittet syftar till och vilka rubriker som hör hit.</p>
       <p>I respektive flik finns det olika rubriker, vissa är obligatoriska och vissa kan du själv välja om du vill ha med eller inte. Under redigering av kurs-PM kan du för respektive rubrik läsa i i-ikonen bredvid rubriken; vad rubriken handlar om, hur den hjälper studenter och hur innehållet redigeras.</p>
       <p>Det finns också möjlighet att lägga till egna rubriker, men tänk på att i så stor utsträckning som möjligt använda de rubriker som finns. Detta eftersom att rubrikerna är framtagna utifrån KTH skolor och studenters behov och att studenter önskar igenkänning mellan kurser.</p>
       <p>Om du valt att redigera en rubrik har du möjlighet att expandera ”Visa vägledning” för att få vägledning hur du ska skriva innehåll under rubriken.</p>`
