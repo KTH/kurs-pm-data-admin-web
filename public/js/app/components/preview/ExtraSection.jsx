7e4b57737f0229b557d1894bbc6a2b77020d6429
Elena Rakhimova <elenara@kth.se>
2021-03-04 22:28:30 +0100

    refactor: ExtraSection component (React hooks, applicationStore)

--- public/js/app/components/preview/ExtraSection.jsx
+++ public/js/app/components/preview/ExtraSection.jsx
@@ -1,62 +1,50 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/no-danger */
-import React, { Component } from 'react'
-import { inject, observer } from 'mobx-react'
+import React from 'react'
+import { observer } from 'mobx-react'
 import { ExtraHeaderHead } from './ContentHead'
+import { useStore } from '../../mobx'
 
 import i18n from '../../../../../i18n'
 
-@inject(['routerStore'])
-@observer
-class ExtraSection extends Component {
-  // eslint-disable-next-line react/state-in-constructor
-  state = {
-    isOpen: this.props.isEmptyNew || false,
-    contentForEditor: this.props.initialValue || '', // this.props.routerStore???
-    contentForTitle: this.props.initialTitle || '', // Default value needed
-    visibleInMemo: this.props.visibleInMemo,
-    isEmptyNew: this.props.isEmptyNew || false
-  }
+function ExtraSection(props) {
+  const { language } = useStore()
+  const {
+    contentId,
+    isEmptyNew,
+    initialValue: contentForEditor = '',
+    initialTitle: contentForTitle = '',
+    memoLanguageIndex,
+    visibleInMemo,
+  } = props
 
-  userLangIndex = this.props.routerStore.language === 'sv' ? 1 : 0
+  userLangIndex = language === 'sv' ? 1 : 0
+  const { sourceInfo } = i18n.messages[this.userLangIndex]
 
-  render() {
-    const { contentId } = this.props // menuId, visibleInMemo
+  return (
+    <span className="Add--New--Title--And--Info">
+      {!isEmptyNew && (
+        <ExtraHeaderHead header={contentForTitle} contentId={contentId} memoLangIndex={memoLanguageIndex} />
+      )}
 
-    const { contentForEditor, contentForTitle, isEmptyNew, visibleInMemo } = this.state
-
-    const { sourceInfo } = i18n.messages[this.userLangIndex]
-
-    return (
-      <span className="Add--New--Title--And--Info">
-        {!this.state.isOpen && !isEmptyNew && (
-          <ExtraHeaderHead
-            header={contentForTitle}
-            contentId={contentId}
-            memoLangIndex={this.props.memoLanguageIndex}
+      {!isEmptyNew &&
+        /* is included in memo, preview text without editor */
+        ((visibleInMemo && (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: (contentForEditor !== '' && contentForEditor) || '',
+            }}
           />
-        )}
-
-        {!this.state.isOpen &&
-          !isEmptyNew &&
-          /* is included in memo, preview text without editor */
-          ((visibleInMemo && (
-            <span
-              dangerouslySetInnerHTML={{
-                __html: (contentForEditor !== '' && contentForEditor) || ''
-              }}
-            />
-          )) ||
-            /* editor has content but is not yet included in pm */
-            (contentForEditor !== '' && ( // TODO: add DEFAULT TEXT
-              <span>
-                <p>
-                  {/* <i>{type === 'optionalEditable' ? sourceInfo.notIncludedInMemoYet : sourceInfo.notIncludedInMemoYetOfAddition}</i> */}
-                </p>
-              </span>
-            )))}
-      </span>
-    )
-  }
+        )) ||
+          /* editor has content but is not yet included in pm */
+          (contentForEditor !== '' && ( // TODO: add DEFAULT TEXT
+            <span>
+              <p>
+                {/* <i>{type === 'optionalEditable' ? sourceInfo.notIncludedInMemoYet : sourceInfo.notIncludedInMemoYetOfAddition}</i> */}
+              </p>
+            </span>
+          )))}
+    </span>
+  )
 }
 export default ExtraSection
