42e5125bc18862ffa6bf4c3d8fae6d6dba1c2901
Elena Rakhimova <elenara@kth.se>
2021-03-09 18:21:22 +0100

    refactor: update app.jsx new store and render structure

--- public/js/app/app.jsx
+++ public/js/app/app.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-// import { StaticRouter } from 'react-router'
-// import { Provider } from 'mobx-react'
 
 // Sass
 import '../../css/node-web.scss'
 
 // Store
-// import RouterStore from './stores/RouterStore'
 import { MobxStoreProvider, uncompressStoreInPlaceFromDocument } from './mobx'
 import createApplicationStore from './stores/createApplicationStore'
 
 // Pages
 import ChangePublished from './pages/ChangePublished'
 import CreateNewMemo from './pages/CreateNewMemo'
-// import MemoContainer from './pages/MemoContainer'
-// import MemoEditingContainer from './pages/MemoEditingContainer'
+import MemoEditingContainer from './pages/MemoEditingContainer'
 import PreviewContainer from './pages/PreviewContainer'
 
 function _renderOnClientSide() {
@@ -26,7 +22,6 @@ function _renderOnClientSide() {
   }
 
   // const basename = window.config.proxyPrefixPath.uri
-  // console.log('basename', basename)
   const applicationStore = createApplicationStore()
   uncompressStoreInPlaceFromDocument(applicationStore)
 
@@ -45,8 +40,7 @@ function appFactory(applicationStore) {
       <Switch>
         <Route exact path="/kursinfoadmin/kurs-pm-data/published/:courseCode/" component={ChangePublished} />
         <Route exact path="/kursinfoadmin/kurs-pm-data/:courseCode/" component={CreateNewMemo} />
-        {/* <Route exact path="/kursinfoadmin/kurs-pm-data/v1/:courseCode/:memoEndPoint" component={MemoContainer} />
-        <Route exact path="/kursinfoadmin/kurs-pm-data/:courseCode/:memoEndPoint" component={MemoEditingContainer} />*/}
+        <Route exact path="/kursinfoadmin/kurs-pm-data/:courseCode/:memoEndPoint" component={MemoEditingContainer} />
         <Route
           exact
           path="/kursinfoadmin/kurs-pm-data/:courseCode/:memoEndPoint/preview"
@@ -56,17 +50,5 @@ function appFactory(applicationStore) {
     </MobxStoreProvider>
   )
 }
-
-// function staticRender(context, location) {
-//   return (
-//     <StaticRouter location={location} context={context}>
-//       {appFactory()}
-//     </StaticRouter>
-//   )
-// }
-
-// if (typeof window !== 'undefined') {
-//   ReactDOM.render(<BrowserRouter>{appFactory()}</BrowserRouter>, document.getElementById('app'))
-// }
 
 export default appFactory
