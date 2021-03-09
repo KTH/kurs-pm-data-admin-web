b60352b9d6cce4d889bf3d03f788284bdbc85fa4
Elena Rakhimova <elenara@kth.se>
2021-03-09 18:24:05 +0100

    refactor: use react hooks in New section editor

--- public/js/app/components/editors/NewSectionEditor.jsx
+++ public/js/app/components/editors/NewSectionEditor.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-danger */
-import React, { Component } from 'react'
-import { inject, observer } from 'mobx-react'
+import React, { useState, useEffect } from 'react'
+import { observer } from 'mobx-react'
+import { useStore } from '../../mobx'
+
 import { Editor } from '@tinymce/tinymce-react'
 import i18n from '../../../../../i18n'
 import { ActionModalButton } from '@kth/kth-kip-style-react-components'
@@ -11,247 +13,186 @@ import { Form, FormGroup, Label, Input } from 'reactstrap'
 import editorConf from '../../util/editorInitConf'
 import PropTypes from 'prop-types'
 
-@inject(['routerStore'])
-@observer
-class NewSectionEditor extends Component {
-  state = {
-    extraContent:
-      this.props.routerStore.memoData[this.props.contentId][this.props.currentIndex] || [],
-    isOpen: false,
-    showEmptyTitleAlert: false
-  }
+function NewSectionEditor(props) {
+  const store = useStore()
+  const { langIndex: userLangIndex, memoLangAbbr, memoData, dirtyEditor } = store
 
-  userLangIndex = this.props.routerStore.langIndex
+  const { currentIndex, contentId, menuId, showError, uKey } = props
 
-  memoLangIndex = this.props.routerStore.memoLangAbbr === 'sv' ? 1 : 0
+  const extraContent = memoData[contentId][currentIndex] || []
+  const { title, htmlContent, visibleInMemo } = extraContent
 
-  static getDerivedStateFromProps(props, state) {
-    const { contentId, currentIndex, showError } = props
-    const { extraContent: initialExtraContent, showEmptyTitleAlert } = state
-    const { extraContent: updatedExtraContent } = props.routerStore.memoData[contentId][
-      currentIndex
-    ]
-    const extraContent = updatedExtraContent || initialExtraContent
-    // fast reaction to props change after switch/submit
-    if (showError && !showEmptyTitleAlert) {
-      return { extraContent, showEmptyTitleAlert: showError }
-    }
+  const hasEmptyTitle = !!(title.length === 0)
+  const hasEmptyText = !!(htmlContent.length === 0)
 
-    // check if hasEmptyTitle error is fixed and update state
-    const { title, htmlContent } = extraContent
-    const hasEmptyTitle = !!(title.length === 0)
-    const hasEmptyText = !!(htmlContent.length === 0)
-    return { extraContent, showEmptyTitleAlert: hasEmptyTitle && !hasEmptyText }
-  }
+  const [isOpen, setOpenStatus] = useState(false)
+  const [showEmptyTitleErrorLabel, setEmptyTitleErrorLabel] = useState(false)
 
-  componentDidMount() {
-    const { extraContent } = this.state
+  const memoLangIndex = memoLangAbbr === 'sv' ? 1 : 0
+  const { actionModals, buttons, sourceInfo, memoInfoByUserLang } = i18n.messages[userLangIndex]
 
-    this.setState({
-      isOpen: extraContent.isEmptyNew || !!(extraContent.title.length === 0) || false
-    })
-  }
-
-  componentDidUpdate() {
-    const { uKey, menuId, contentId, currentIndex, routerStore, showError } = this.props
-    const { extraContent, showEmptyTitleAlert } = this.state
-    // eslint-disable-next-line react/destructuring-assignment
-    this.updateExtraHeadersState(contentId, currentIndex, extraContent)
-    routerStore.dirtyEditor = uKey
-
-    if (showError && showEmptyTitleAlert) {
+  useEffect(() => {
+    //fast reaction to parent prop showError change when user clicked switch tab/submit
+    if (showError && !showEmptyTitleErrorLabel) {
+      setEmptyTitleErrorLabel(true)
       const sectionElement = document.getElementById(menuId)
       sectionElement.scrollIntoView({ behavior: 'smooth' })
     }
+  }, [showError, showEmptyTitleErrorLabel])
+
+  useEffect(() => {
+    // check if hasEmptyTitle error is fixed and update state
+    setEmptyTitleErrorLabel(hasEmptyTitle && !hasEmptyText)
+  }, [title, htmlContent])
+
+  useEffect(() => {
+    // decides to (keep) editor open if title it empty
+    console.log('new editor hall it open')
+    if (hasEmptyTitle) setOpenStatus(true)
+  }, [hasEmptyTitle])
+
+  useEffect(() => {
+    // eslint-disable-next-line react/destructuring-assignment
+    console.log('save new section data')
+    store.checkTitleExist(contentId, currentIndex, hasEmptyTitle, hasEmptyText)
+  }, [title])
+
+  useEffect(() => {
+    store.setDirtyEditor(uKey)
+  }, [title, htmlContent, visibleInMemo])
+
+  const setNewContent = editorContent => {
+    store.setMemoExtraContent(contentId, currentIndex, 'htmlContent', editorContent.trim())
   }
 
-  componentWillUnmount() {
-    this.onSaveByThisContentId()
-  }
-
-  updateExtraHeadersState = () => {
-    const { contentId, currentIndex } = this.props
-    const { extraContent } = this.state
-    const { title, htmlContent } = extraContent
-    const hasEmptyTitle = !!(title.length === 0)
-    const hasEmptyText = !!(htmlContent.length === 0)
-    // full path to routerStore
-    this.props.routerStore.updateThisExtraState(
-      contentId,
-      currentIndex,
-      hasEmptyTitle,
-      hasEmptyText
-    )
-  }
-
-  setNewContent = (editorContent) => {
-    const { contentId, currentIndex } = this.props
-    const { memoData } = this.props.routerStore
-    memoData[contentId][currentIndex].htmlContent = editorContent.trim()
-  }
-
-  setNewTitle = (event) => {
+  const setNewTitle = event => {
     event.preventDefault()
-    const { contentId, currentIndex } = this.props
-    const { memoData } = this.props.routerStore
     const title = event.target.value.trim()
-    memoData[contentId][currentIndex].title = title // || (this.memoLangIndex === 1 ? 'Egna rubrik ' + currentIndex : 'New heading ' + currentIndex)
+    store.setMemoExtraContent(contentId, currentIndex, 'title', title) // || (this.memoLangIndex === 1 ? 'Egna rubrik ' + currentIndex : 'New heading ' + currentIndex)
   }
 
-  onRemoveThisContent = (wasEmpty = false) => {
-    const { contentId, uKey, currentIndex } = this.props
-    this.props.routerStore.dirtyEditor = uKey
-    this.props.routerStore.removeExtraContent(contentId, currentIndex)
-    if (wasEmpty) this.props.onAlert('removedEmptyContent', 'success', 500)
-    else this.props.onAlert('removedAddedContent', 'success', 500)
+  const onRemoveThisContent = (wasEmpty = false) => {
+    store.setDirtyEditor(uKey)
+    store.removeExtraContent(contentId, currentIndex)
+    if (wasEmpty) props.onAlert('removedEmptyContent', 'success', 500)
+    else props.onAlert('removedAddedContent', 'success', 500)
+    //onSaveByThisContentId
   }
 
-  onSaveByThisContentId = () => {
-    const { contentId, currentIndex, uKey } = this.props
-    const { dirtyEditor } = this.props.routerStore
-    const latestMemoData = this.props.routerStore.memoData[contentId]
+  const onSaveByThisContentId = () => {
+    const latestMemoData = store.memoData[contentId]
     // thisSectionExist is needed to know if section was deleted before unmounting
-    const thisSectionExist = !!this.props.routerStore.memoData[contentId].length > currentIndex
+    const thisSectionExist = !!store.memoData[contentId].length > currentIndex
 
     if (dirtyEditor === uKey) {
-      this.props.onSave({ [contentId]: latestMemoData }, 'autoSaved')
+      props.onSave({ [contentId]: latestMemoData }, 'autoSaved')
     }
-    this.props.routerStore.dirtyEditor = ''
-
-    if (thisSectionExist) delete this.props.routerStore.memoData[contentId][currentIndex].isEmptyNew
+    store.setDirtyEditor('')
   }
 
-  toggleVisibleInMemo = () => {
-    const { contentId, currentIndex } = this.props
-    const { memoData } = this.props.routerStore
-    const { visibleInMemo } = memoData[contentId][currentIndex]
-    this.props.routerStore.memoData[contentId][currentIndex].visibleInMemo = !visibleInMemo
-    this.onSaveByThisContentId()
+  const toggleVisibleInMemo = () => {
+    store.setMemoExtraContent(contentId, currentIndex, 'visibleInMemo', !visibleInMemo)
+    onSaveByThisContentId()
   }
 
-  onToggleVisibleEditor = () => {
-    const { contentId, currentIndex, uKey } = this.props
-    const { isOpen } = this.state
-    const { memoData } = this.props.routerStore
-    const { title, htmlContent } = memoData[contentId][currentIndex]
-    const hasEmptyTitle = !!(title.length === 0)
-    const hasEmptyText = !!(htmlContent.length === 0)
+  const onToggleVisibleEditor = () => {
     if (isOpen) {
-      if (
-        hasEmptyTitle && // memoData[contentId][currentIndex].title.trim().length === 0 &&
-        hasEmptyText
-      ) {
-        this.onRemoveThisContent(true)
+      if (hasEmptyTitle && hasEmptyText) {
+        onRemoveThisContent(true)
 
         return false
       }
       if (hasEmptyTitle) {
-        this.setState({ showEmptyTitleAlert: true })
-        this.props.onAlert('errorEmptyTitle', 'danger')
+        setEmptyTitleErrorLabel(true)
+        props.onAlert('errorEmptyTitle', 'danger')
 
         return false
       }
-      delete this.props.routerStore.memoData[contentId][currentIndex].isEmptyNew
-    } else this.props.routerStore.dirtyEditor = uKey
+    } else store.setDirtyEditor(uKey)
 
-    this.setState({ isOpen: !isOpen })
+    setOpenStatus(!isOpen)
     return true
   }
 
-  render() {
-    const { uKey, contentId, menuId } = this.props
-    const { userLangIndex } = this
-    const { extraContent, showEmptyTitleAlert, isOpen } = this.state
-    const { htmlContent, title, isEmptyNew, visibleInMemo } = extraContent
-
-    const { actionModals, buttons, sourceInfo, memoInfoByUserLang } = i18n.messages[userLangIndex]
-
-    return (
-      <span id={menuId} className="Add--New--Title--And--Info">
-        {!isOpen && !isEmptyNew && (
-          <ExtraHeaderHead
-            header={title}
-            contentId={contentId}
-            memoLangIndex={this.memoLangIndex}
-            userLangIndex={userLangIndex}
-          />
-        )}
-
-        <VisibilityInfo
-          contentId={`${contentId}-${uKey}`}
-          sectionType="section"
-          visibleInMemo={visibleInMemo}
-          onToggleVisibleInMemo={this.toggleVisibleInMemo}
-          isEditorOpen={isOpen || isEmptyNew}
-          onToggleVisibleEditor={this.onToggleVisibleEditor}
+  return (
+    <span id={menuId} className="Added--New--Title--And--Info">
+      {!isOpen && (
+        <ExtraHeaderHead
+          header={title}
+          contentId={contentId}
+          memoLangIndex={memoLangIndex}
           userLangIndex={userLangIndex}
         />
-        {(isEmptyNew || isOpen) && (
-          <span>
-            <Form className={showEmptyTitleAlert ? 'error-area' : ''}>
-              <FormGroup className="title">
-                <Label className="form-control-label" htmlFor={`headerFor${contentId}-${uKey}`}>
-                  {sourceInfo.addNewTitle}
-                </Label>
-                <Input
-                  className="form-control"
-                  type="text"
-                  id={`headerFor${contentId}-${uKey}`}
-                  onChange={this.setNewTitle}
-                  onBlur={this.onSaveByThisContentId}
-                  defaultValue={title}
-                />
-                {showEmptyTitleAlert && (
-                  <Label htmlFor={`headerFor${contentId}-${uKey}`} className="error-label">
-                    {sourceInfo.errorEmptyTitle}
-                  </Label>
-                )}
-              </FormGroup>
-            </Form>
-            <CollapseGuidance
-              title={buttons.showGuidance}
-              details={memoInfoByUserLang[contentId].help}
-            />
-            <Editor
-              id={`editorFor${contentId}-${uKey}`}
-              initialValue={htmlContent}
-              init={editorConf(userLangIndex === 1 ? 'sv_SE' : null)}
-              onEditorChange={this.setNewContent}
-              onBlur={this.onSaveByThisContentId}
-            />
-            <ActionModalButton
-              btnLabel={buttons.btnRemoveHeader}
-              modalId={`beforeRemoving-${contentId}-${uKey}`}
-              type="remove"
-              modalLabels={actionModals.newSectionRemove}
-              onConfirm={this.onRemoveThisContent}
-            />
-          </span>
-        )}
+      )}
 
-        {!isOpen &&
-          !isEmptyNew &&
-          /* is included in memo, preview text without editor */
-          ((visibleInMemo && (
-            <span
-              dangerouslySetInnerHTML={{
-                __html:
-                  (htmlContent !== '' && htmlContent) ||
-                  `<p><i>${sourceInfo.noInfoYet.section}</i></p>`
-              }}
-            />
-          )) ||
-            /* editor has content but is not yet included in pm */
-            (htmlContent !== '' && ( // TODO: add DEFAULT TEXT
-              <span>
-                <p>
-                  <i>{sourceInfo.notIncludedInMemoYet.section}</i>
-                </p>
-              </span>
-            )))}
-      </span>
-    )
-  }
+      <VisibilityInfo
+        contentId={`${contentId}-${uKey}`}
+        sectionType="section"
+        visibleInMemo={visibleInMemo}
+        onToggleVisibleInMemo={toggleVisibleInMemo}
+        isEditorOpen={isOpen}
+        onToggleVisibleEditor={onToggleVisibleEditor}
+        userLangIndex={userLangIndex}
+      />
+      {isOpen && (
+        <span>
+          <Form className={showEmptyTitleErrorLabel ? 'error-area' : ''}>
+            <FormGroup className="title">
+              <Label className="form-control-label" htmlFor={`headerFor${contentId}-${uKey}`}>
+                {sourceInfo.addNewTitle}
+              </Label>
+              <Input
+                className="form-control"
+                type="text"
+                id={`headerFor${contentId}-${uKey}`}
+                onChange={setNewTitle}
+                onBlur={onSaveByThisContentId}
+                defaultValue={title}
+              />
+              {showEmptyTitleErrorLabel && (
+                <Label htmlFor={`headerFor${contentId}-${uKey}`} className="error-label">
+                  {sourceInfo.errorEmptyTitle}
+                </Label>
+              )}
+            </FormGroup>
+          </Form>
+          <CollapseGuidance title={buttons.showGuidance} details={memoInfoByUserLang[contentId].help} />
+          <Editor
+            id={`editorFor${contentId}-${uKey}`}
+            initialValue={htmlContent}
+            init={editorConf(userLangIndex === 1 ? 'sv_SE' : null)}
+            onEditorChange={setNewContent}
+            onBlur={onSaveByThisContentId}
+          />
+          <ActionModalButton
+            btnLabel={buttons.btnRemoveHeader}
+            modalId={`beforeRemoving-${contentId}-${uKey}`}
+            type="remove"
+            modalLabels={actionModals.newSectionRemove}
+            onConfirm={onRemoveThisContent}
+          />
+        </span>
+      )}
+
+      {!isOpen &&
+        /* is included in memo, preview text without editor */
+        ((visibleInMemo && (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: (htmlContent !== '' && htmlContent) || `<p><i>${sourceInfo.noInfoYet.section}</i></p>`,
+            }}
+          />
+        )) ||
+          /* editor has content but is not yet included in pm */
+          (htmlContent !== '' && ( // TODO: add DEFAULT TEXT
+            <span>
+              <p>
+                <i>{sourceInfo.notIncludedInMemoYet.section}</i>
+              </p>
+            </span>
+          )))}
+    </span>
+  )
 }
 
 NewSectionEditor.propTypes = {
@@ -262,14 +203,12 @@ NewSectionEditor.propTypes = {
   visibleInMemo: PropTypes.bool,
   onAlert: PropTypes.func.isRequired,
   onSave: PropTypes.func.isRequired,
-  // eslint-disable-next-line react/require-default-props
-  routerStore: PropTypes.func,
   showError: PropTypes.bool,
-  uKey: PropTypes.string.isRequired
+  uKey: PropTypes.string.isRequired,
 }
 
 NewSectionEditor.defaultProps = {
-  htmlContent: ''
+  htmlContent: '',
 }
 
-export default NewSectionEditor
+export default observer(NewSectionEditor)
