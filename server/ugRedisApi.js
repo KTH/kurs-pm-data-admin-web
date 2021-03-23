c7c45060c3bceb36ac7b4b27873eccba4363d894
Elena Rakhimova <elenara@kth.se>
2021-03-23 14:53:26 +0100

    chore: lint

--- server/ugRedisApi.js
+++ server/ugRedisApi.js
@@ -1,30 +1,24 @@
-const serverConfig = require('./configuration').server
 const redis = require('kth-node-redis')
-// const i18n = require('../i18n')
 const log = require('kth-node-log')
+const { server: serverConfig } = require('./configuration')
 
-const redisKeys = (courseCode, semester, ladokRoundIds) => {
-  // Used to get examiners and responsibles from UG Rdedis
-  return {
-    teachers: ladokRoundIds.map((round) => `${courseCode}.${semester}.${round}.teachers`),
-    examiner: [`${courseCode}.examiner`],
-    responsibles: ladokRoundIds.map(
-      (round) => `${courseCode}.${semester}.${round}.courseresponsible`
-    ),
-    assistants: ladokRoundIds.map((round) => `${courseCode}.${semester}.${round}.assistants`) // edu.courses.SF.SF1624.20191.1.assistants
-  }
-}
+const redisKeys = (courseCode, semester, ladokRoundIds) => ({
+  teachers: ladokRoundIds.map(round => `${courseCode}.${semester}.${round}.teachers`),
+  examiner: [`${courseCode}.examiner`],
+  responsibles: ladokRoundIds.map(round => `${courseCode}.${semester}.${round}.courseresponsible`),
+  assistants: ladokRoundIds.map(round => `${courseCode}.${semester}.${round}.assistants`), // edu.courses.SF.SF1624.20191.1.assistants
+})
 
-const _removeDublicates = (personListWithDublicates) =>
+const _removeDublicates = personListWithDublicates =>
   personListWithDublicates
-    .map((person) => JSON.stringify(person))
+    .map(person => JSON.stringify(person))
     .filter((person, index, self) => self.indexOf(person) === index)
-    .map((personStr) => JSON.parse(personStr))
+    .map(personStr => JSON.parse(personStr))
 
-const createPersonHtml = (personList) => {
+const createPersonHtml = personList => {
   let personString = ''
   personList &&
-    personList.forEach((person) => {
+    personList.forEach(person => {
       if (person) {
         personString += `<p class = "person">
       <img src="https://www.kth.se/files/thumbnail/${person.username}" alt="Profile picture" width="31" height="31">
@@ -41,11 +35,7 @@ const createPersonHtml = (personList) => {
 async function _getCourseEmployees(apiMemoData) {
   const { courseCode, semester, ladokRoundIds } = apiMemoData
   try {
-    const { assistants, teachers, examiner, responsibles } = redisKeys(
-      courseCode,
-      semester,
-      ladokRoundIds
-    )
+    const { assistants, teachers, examiner, responsibles } = redisKeys(courseCode, semester, ladokRoundIds)
     log.info(
       '-------------------=> _getCourseEmployees for all memos course rounds with keys: ',
       assistants,
@@ -62,10 +52,8 @@ async function _getCourseEmployees(apiMemoData) {
       .mget(assistants) // [3]
       .execAsync()
     log.info('Ug Redis fetched correctly, example >>> Teachers: ', arrWithStringifiedArrays[1])
-    const flatArrWithHtmlStr = arrWithStringifiedArrays.map((perTypeStringifiedArr) => {
-      const thisTypeAllRoundsEmployees = perTypeStringifiedArr.flatMap((perRoundStr) =>
-        JSON.parse(perRoundStr)
-      )
+    const flatArrWithHtmlStr = arrWithStringifiedArrays.map(perTypeStringifiedArr => {
+      const thisTypeAllRoundsEmployees = perTypeStringifiedArr.flatMap(perRoundStr => JSON.parse(perRoundStr))
       /* Remove duplicates */
       const deepDistinctEmpoyeesNoDublicates = _removeDublicates(thisTypeAllRoundsEmployees)
       return createPersonHtml(deepDistinctEmpoyeesNoDublicates)
@@ -74,7 +62,7 @@ async function _getCourseEmployees(apiMemoData) {
       teacher: flatArrWithHtmlStr[0],
       examiner: flatArrWithHtmlStr[1],
       courseCoordinator: flatArrWithHtmlStr[2],
-      teacherAssistants: flatArrWithHtmlStr[3]
+      teacherAssistants: flatArrWithHtmlStr[3],
     }
   } catch (err) {
     log.error('Exception from ugRedis - multi', { error: err })
@@ -83,5 +71,5 @@ async function _getCourseEmployees(apiMemoData) {
 }
 
 module.exports = {
-  getCourseEmployees: _getCourseEmployees
+  getCourseEmployees: _getCourseEmployees,
 }
