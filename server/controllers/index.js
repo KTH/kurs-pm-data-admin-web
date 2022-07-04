bf55519278b510354540ed9fcee245c40268910e
Elena Rakhimova <elenara@kth.se>
2022-07-04 15:36:56 +0200

    fix: add home page instead of about page

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,6 +6,7 @@
  */
 
 module.exports = {
+  NoCourseCodeCtrl: require('./noCourseCodeCtrl'),
   System: require('./systemCtrl'),
   MemoContent: require('./memoCtrl'),
   ChooseMemoStartPoint: require('./choiceOptionsCtrl'),
