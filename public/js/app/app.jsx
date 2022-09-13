327592145ff7f603a934b104373624792aac4d5d
Elena Rakhimova <elenara@kth.se>
2022-09-13 11:13:30 +0200

    fix: add ErrorBoundaries

--- public/js/app/app.jsx
+++ public/js/app/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
 
 // Sass
 import '../../css/node-web.scss'
@@ -15,6 +16,17 @@ import CreateNewMemo from './pages/CreateNewMemo'
 import MemoEditingContainer from './pages/MemoEditingContainer'
 import PreviewContainer from './pages/PreviewContainer'
 import EditorsForTest from './components/editors/test/EditorsForTest'
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  console.error(error)
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
 
 function appFactory(applicationStore) {
   return (
@@ -45,7 +57,18 @@ function _renderOnClientSide() {
   uncompressStoreInPlaceFromDocument(applicationStore)
 
   // Removed basename because it is causing empty string basename={basename}
-  const app = <BrowserRouter>{appFactory(applicationStore)}</BrowserRouter>
+  const app = (
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => {
+        location.reload()
+        // reset the state of your app so the error doesn't happen again
+      }}
+    >
+      <BrowserRouter>{appFactory(applicationStore)}</BrowserRouter>
+    </ErrorBoundary>
+  )
+
   const domElement = document.getElementById('app')
   // ReactDOM.hydrate(app, domElement)
   ReactDOM.render(app, domElement)
