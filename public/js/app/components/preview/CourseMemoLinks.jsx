5d13d3f2bd12813083191c3f77102eea5870656a
Martina Thege <martinathege@gmail.com>
2022-12-15 11:53:35 +0100

    feat: ajusted border-bottom in tabs/navbar and changed position of printer icon

--- public/js/app/components/preview/CourseMemoLinks.jsx
+++ public/js/app/components/preview/CourseMemoLinks.jsx
@@ -48,10 +48,10 @@ const archiveLink = (language, labels, courseCode) => (
 const pdfLink = labels => (
   <span id="print-link-with-popup">
     <h4>{labels.courseMemoPrint}</h4>
-    <Button id="print-pm-link" color="link">
+    <Button className="print-pm-link" id="print-pm-link" color="link">
+      <svg className="print-icon" />
       {labels.linkCourseMemoPrint}
     </Button>
-    <svg className="print-icon" />
     <Popup header={labels.courseMemoPrint} body={labels.courseMemoModal} targetId="print-pm-link" />
   </span>
 )
