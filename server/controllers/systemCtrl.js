682c11ceabee24eb2a134ba846d7014c81f6c107
Benjamin Langlotz <benjala@kth.se>
2023-08-28 14:49:14 +0200

    chore(deps): update kth-monitor to v4

--- server/controllers/systemCtrl.js
+++ server/controllers/systemCtrl.js
@@ -5,7 +5,7 @@ const os = require('os')
 const errorHandler = require('@kth/kth-node-web-common/lib/error')
 const { getPaths } = require('kth-node-express-routing')
 const language = require('@kth/kth-node-web-common/lib/language')
-const monitorSystems = require('@kth/monitor')
+const { monitorRequest } = require('@kth/monitor')
 const log = require('@kth/log')
 const redis = require('kth-node-redis')
 const version = require('../../config/version')
@@ -146,7 +146,7 @@ function _about(req, res) {
 async function _monitor(req, res) {
   try {
     const apiConfig = config.nodeApi
-    await monitorSystems(req, res, [
+    await monitorRequest(req, res, [
       ...(api
         ? Object.keys(api).map(apiKey => ({
             key: apiKey,
@@ -159,16 +159,6 @@ async function _monitor(req, res) {
         required: true,
         redis,
         options: config.session.redisOptions,
-      },
-      // If we need local system checks, such as memory or disk, we would add it here.
-      // Make sure it returns an object containing:
-      // {key: 'local', isResolved: true, statusCode: ###, message: '...'}
-      // The property statusCode should be standard HTTP status codes.
-      {
-        key: 'local',
-        isResolved: true,
-        message: '- local system checks: OK',
-        statusCode: 200,
       },
     ])
   } catch (error) {
