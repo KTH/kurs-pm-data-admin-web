b7e6d24dc8f8f4bb766b79d19f253e8c7f68e209
Karl Andin <karl.andin@drakryggen.com>
2024-05-27 16:10:19 +0200

    feat(KUI-1266): move toolbar url to env variable

--- server/controllers/choiceOptionsCtrl.js
+++ server/controllers/choiceOptionsCtrl.js
@@ -83,6 +83,7 @@ async function getCourseOptionsPage(req, res, next) {
         title: i18n.messages[langIndex === 0 ? 1 : 0].messages.locale_text,
         toLang: `?l=${lang === 'sv' ? 'en' : 'sv'}`,
       },
+      toolbarUrl: server.toolbar.url,
       proxyPrefix,
       description:
         lang === 'sv'
