3b97765a713b5aae98a677447067d32674af8c58
Karl Andin <karl.andin@drakryggen.com>
2024-06-03 20:04:39 +0200

    fix(KUI-1266): fix issue with Alert by supporting custom classnames

--- public/js/app/components-shared/Alert.jsx
+++ public/js/app/components-shared/Alert.jsx
@@ -4,12 +4,13 @@
  * Changelog:
  * - 2024-04- - initial version (release of kth-style 10 public apps)
  * - 2024-05-31 - add aria-live="polite" as default and add {...rest} to allow adding extra props (as test-ids) to Alert root element
+ * - 2024-06-03 - add support for classname prop
  */
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Alert = ({ type, header, children, ...rest }) => (
-  <div className={`kth-alert ${type}`} role="alert" aria-live="polite" {...rest}>
+const Alert = ({ type, header, children, className, ...rest }) => (
+  <div className={`kth-alert ${type} ${className ?? ''}`} role="alert" aria-live="polite" {...rest}>
     {header && <h4>{header}</h4>}
     <div>{children}</div>
   </div>
