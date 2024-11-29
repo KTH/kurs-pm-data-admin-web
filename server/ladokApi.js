299b96faf15f41d368336d354897a2d5ef98f492
Axel Björkqvist <axelbjo@kth.se>
2024-11-29 15:18:40 +0100

    fix(KUI-1523): changed mapping of language, removed campus field

--- server/ladokApi.js
+++ server/ladokApi.js
@@ -19,25 +19,18 @@ async function getCourseRoundsData(courseCode, lang) {
   // TODO: need to find a way to handle state instead of just hardcoding it
   try {
     const course = await client.getActiveCourseRounds(courseCode, lang)
-    const mappedRounds = course.map(round => {
-      const otherLang = lang === 'en' ? 'sv' : 'en'
-      return {
-        shortName: round.kortnamn,
-        applicationCode: round.tillfalleskod,
-        startperiod: round.startperiod,
-        firstTuitionDate: round.forstaUndervisningsdatum.date,
-        lastTuitionDate: round.sistaUndervisningsdatum.date,
-        state: 'APPROVED',
-        language: {
-          [lang]: round.undervisningssprak ? round.undervisningssprak.name : '',
-          [otherLang]: round.undervisningssprak ? round.undervisningssprak.nameOther : '',
-        },
-        campus: {
-          [lang]: round.studieort ? round.studieort.name : '',
-          [otherLang]: round.studieort ? round.studieort.nameOther : '',
-        },
-      }
-    })
+    const mappedRounds = course.map(round => ({
+      shortName: round.kortnamn,
+      applicationCode: round.tillfalleskod,
+      startperiod: round.startperiod,
+      firstTuitionDate: round.forstaUndervisningsdatum.date,
+      lastTuitionDate: round.sistaUndervisningsdatum.date,
+      state: 'APPROVED',
+      language: {
+        sv: (lang === 'sv' ? round.undervisningssprak?.name : round.undervisningssprak?.nameOther) ?? '',
+        en: (lang === 'en' ? round.undervisningssprak?.name : round.undervisningssprak?.nameOther) ?? '',
+      },
+    }))
     return mappedRounds
   } catch (error) {
     throw new Error(error.message)
