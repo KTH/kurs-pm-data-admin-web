696d3c1aef2ff2ee086589a52d403ef1f9d68779
Charlotta Jaunviksna <charlotta.jaunviksna@precisit.com>
2025-05-19 16:04:05 +0200

    fix(KUI-1651): add optional chaining operators where needed

--- server/utils/formatLadokData.js
+++ server/utils/formatLadokData.js
@@ -24,8 +24,8 @@ const formatLadokData = (ladokCourseRounds, ladokCourseData) => {
     course: {
       title: ladokCourseData.benamning,
       credits: ladokCourseData.omfattning,
-      creditUnitLabel: ladokCourseData.utbildningstyp.creditsUnit,
-      creditUnitAbbr: ladokCourseData.utbildningstyp.creditsUnit.code.toLowerCase(),
+      creditUnitLabel: ladokCourseData.utbildningstyp?.creditsUnit,
+      creditUnitAbbr: ladokCourseData.utbildningstyp?.creditsUnit.code.toLowerCase(),
       state: 'ESTABLISHED', // TODO: Handle state dynamically instead of hardcoding it
     },
     lastTermsInfo: groupedLadokCourseRounds,
